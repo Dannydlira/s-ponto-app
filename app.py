ccb733814b5b89955449e89bebd49d1dda3d4723
Dannydlira <dannydlira@gmail.com>
2025-06-21 13:09:02 -0300

    Preparando para deploy no Render

--- app.py
+++ app.py
@@ -45,5 +45,7 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(host="0.0.0.0", port=5000)
+import os
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
