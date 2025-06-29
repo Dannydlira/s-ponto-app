96ad09ac40abc08929ae399ad998f110722e64e5
Dannydlira <dannydlira@gmail.com>
2025-06-29 15:48:16 -0300

    Fix: define Python 3.11 to avoid psycopg2 issue

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ app.secret_key = os.getenv("SECRET_KEY", "chave_padrao")
 
 # Corrigir o prefixo da DATABASE_URL, se necessário
 db_url = os.environ.get("DATABASE_URL")
+
 if db_url and db_url.startswith("postgres://"):
-    db_url = db_url.replace("postgres://", "postgresql://", 1)
+    db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
